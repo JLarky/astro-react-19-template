a558fc019918065ba64217ffa0430a9826499d1a
JLarky <jlarky@gmail.com>
2024-05-19 14:03:23 -0600

    bun astro add node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
   integrations: [react()],
-  output: "hybrid", // or 'server'
+  output: "hybrid",
   experimental: {
     actions: true,
   },
+  adapter: node({
+    mode: "standalone",
+  }),
 });
